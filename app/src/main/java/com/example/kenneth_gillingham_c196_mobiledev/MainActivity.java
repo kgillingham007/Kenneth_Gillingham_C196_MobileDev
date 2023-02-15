66224dac25dc83ecec1ee7bd53dd44d66aed1d24
KennyG <kenneth.gillingham07@gmail.com>
2023-02-15 08:28:37 -0800

    Initial commit

---
+++ app/src/main/java/com/example/kenneth_gillingham_c196_mobiledev/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kenneth_gillingham_c196_mobiledev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
